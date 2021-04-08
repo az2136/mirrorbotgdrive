ab78b0e3d3f6889ea15e3ea2ab8c31f4e51777dc
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-04-08 12:35:23 +0700

    Added /unauthorize command in help

--- bot/__main__.py
+++ bot/__main__.py
@@ -108,6 +108,8 @@ def bot_help(update, context):
 
 /{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by owner of the bot)
 
+/{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the bot (Can only be invoked by owner of the bot)
+
 /{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
 
 /{BotCommands.UsageCommand}: To see Heroku Dyno Stats (Owner only).
